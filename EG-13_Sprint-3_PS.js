a6074f6a7c3b1aa96e9cc1a7f81f3313157ed89d
amitchandradas2004 <amitcdas792@gmail.com>
2026-09-04 00:10:28 +0600

    Problem 5 is solved

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -112,7 +112,18 @@ var canConstruct = function (ransomNote, magazine) {
  * @return {number}
  */
 var majorityElement = function (nums) {
-  // your code goes here
+  let candidate = null;
+  let count = 0;
+
+  for (const num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+
+    count += num === candidate ? 1 : -1;
+  }
+
+  return candidate;
 };
 
 // Expected Input: [2, 2, 1, 1, 1, 2, 2]
