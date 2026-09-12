913398dea478eed7e425cd0156c188b145db9eec
amitchandradas2004 <amitcdas792@gmail.com>
2026-09-12 17:31:51 +0600

    Sprint 4 problem 8 is solved

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -249,7 +249,52 @@ var removeNthFromEnd = function (head, n) {
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function (nums, target) {};
+var searchRange = function (nums, target) {
+  const findFirst = () => {
+    let left = 0;
+    let right = nums.length - 1;
+    let result = -1;
+
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (nums[mid] === target) {
+        result = mid;
+        right = mid - 1;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return result;
+  };
+
+  const findLast = () => {
+    let left = 0;
+    let right = nums.length - 1;
+    let result = -1;
+
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (nums[mid] === target) {
+        result = mid;
+        left = mid + 1;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return result;
+  };
+
+  return [findFirst(), findLast()];
+};
+console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
 // Expected Input: [5, 7, 7, 8, 8, 10], target = 8
 // Expected Output: [3, 4]
 
