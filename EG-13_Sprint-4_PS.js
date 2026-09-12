96ddbd7f1fbf6713262a376f96f0ed0d7c21cb14
amitchandradas2004 <amitcdas792@gmail.com>
2026-09-12 17:51:35 +0600

    Sprint 4 problem 9 is solved

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -305,7 +305,44 @@ console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
  * @param {string} s2
  * @return {boolean}
  */
-var checkInclusion = function (s1, s2) {};
+var checkInclusion = function (s1, s2) {
+  if (s1.length > s2.length) {
+    return false;
+  }
+
+  const count1 = new Array(26).fill(0);
+  const count2 = new Array(26).fill(0);
+
+  // Count characters in s1
+  for (const char of s1) {
+    count1[char.charCodeAt(0) - 97]++;
+  }
+
+  // Create the first window
+  for (let i = 0; i < s1.length; i++) {
+    count2[s2.charCodeAt(i) - 97]++;
+  }
+
+  // Compare first window
+  if (count1.join("") === count2.join("")) {
+    return true;
+  }
+
+  // Sliding window
+  for (let i = s1.length; i < s2.length; i++) {
+    // Add new character
+    count2[s2.charCodeAt(i) - 97]++;
+
+    // Remove old character
+    count2[s2.charCodeAt(i - s1.length) - 97]--;
+
+    if (count1.join("") === count2.join("")) {
+      return true;
+    }
+  }
+
+  return false;
+};
 // Expected Input: "ab", "eidbaooo"
 // Expected Output: true
 // 10. Find All Anagrams in a String
