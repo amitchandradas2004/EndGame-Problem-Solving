fb2d8c3efd8a1d1a6784c53768867812fad1f906
amitchandradas2004 <amitcdas792@gmail.com>
2026-09-12 16:51:32 +0600

    Sprint 4 problem 2 is solved

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -40,7 +40,36 @@ var isIsomorphic = function (s, t) {
  * @param {string} s
  * @return {boolean}
  */
-var wordPattern = function (pattern, s) {};
+var wordPattern = function (pattern, s) {
+  const words = s.split(" ");
+
+  if (pattern.length !== words.length) {
+    return false;
+  }
+
+  const charToWord = {};
+  const wordToChar = {};
+
+  for (let i = 0; i < pattern.length; i++) {
+    const char = pattern[i];
+    const word = words[i];
+
+    // Pattern character already has a different word
+    if (charToWord[char] && charToWord[char] !== word) {
+      return false;
+    }
+
+    // Word already belongs to a different character
+    if (wordToChar[word] && wordToChar[word] !== char) {
+      return false;
+    }
+
+    charToWord[char] = word;
+    wordToChar[word] = char;
+  }
+
+  return true;
+};
 // Expected Input: "abba", "dog cat cat dog"
 // Expected Output: true
 // 03. Find the Difference
