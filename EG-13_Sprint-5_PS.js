9d2b40840ecc58f213b7a0b17ee10a725572448e
amitchandradas2004 <amitcdas792@gmail.com>
2026-09-18 09:07:09 +0600

    Sprint 5 problem 2 is solved

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -32,8 +32,26 @@ var checkSubarraySum = function (nums, k) {
  * @return {number}
  */
 var search = function (nums, target) {
-  // your code goes here
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
 };
+// console.log(search([-1, 0, 3, 5, 9, 12], 9));
 
 // Expected Input: [-1, 0, 3, 5, 9, 12], target = 9
 // Expected Output: 4
