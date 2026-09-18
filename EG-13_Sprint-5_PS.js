4995d27cb772d207e1c7b6d81d189349524e9f34
amitchandradas2004 <amitcdas792@gmail.com>
2026-09-18 09:16:56 +0600

    Sprint 5 problem 7 is solved

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -215,9 +215,37 @@ rotate(nums, 3);
  * @return {void}
  */
 var MinStack = function () {
-  // your code goes here
+  this.stack = [];
+  this.minStack = [];
 };
 
+MinStack.prototype.push = function (val) {
+  this.stack.push(val);
+
+  if (
+    this.minStack.length === 0 ||
+    val <= this.minStack[this.minStack.length - 1]
+  ) {
+    this.minStack.push(val);
+  }
+};
+
+MinStack.prototype.pop = function () {
+  let value = this.stack.pop();
+
+  if (value === this.minStack[this.minStack.length - 1]) {
+    this.minStack.pop();
+  }
+};
+
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+MinStack.prototype.getMin = function () {
+  return this.minStack[this.minStack.length - 1];
+};
+// console.log(MinStack);
 /**
  * @param {number} val
  * @return {void}
