4440f36b3e23d281fa1247454e181bab372be53b
amitchandradas2004 <amitcdas792@gmail.com>
2026-09-18 09:07:50 +0600

    Sprint 5 problem 3 is solved

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -63,8 +63,26 @@ var search = function (nums, target) {
  * @return {number}
  */
 var searchInsert = function (nums, target) {
-  // your code goes here
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
 };
+// console.log(searchInsert([1, 3, 5, 6], 5));
 
 // Expected Input: [1, 3, 5, 6], target = 5
 // Expected Output: 2
