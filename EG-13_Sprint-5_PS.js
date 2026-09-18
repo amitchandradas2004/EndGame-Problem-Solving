a131d6e4617e6a24f85e6bc71670d8ff084e5756
amitchandradas2004 <amitcdas792@gmail.com>
2026-09-18 09:15:13 +0600

    Sprint 5 problem 6 is solved

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -183,8 +183,29 @@ var productExceptSelf = function (nums) {
  * @return {void}
  */
 var rotate = function (nums, k) {
-  // your code goes here
+  k = k % nums.length;
+
+  function reverse(left, right) {
+    while (left < right) {
+      let temp = nums[left];
+      nums[left] = nums[right];
+      nums[right] = temp;
+
+      left++;
+      right--;
+    }
+  }
+
+  reverse(0, nums.length - 1);
+  reverse(0, k - 1);
+  reverse(k, nums.length - 1);
 };
+
+let nums = [1, 2, 3, 4, 5, 6, 7];
+
+rotate(nums, 3);
+
+// console.log(nums);
 
 // Expected Input: [1, 2, 3, 4, 5, 6, 7], k = 3
 // Expected Output: [5, 6, 7, 1, 2, 3, 4]
