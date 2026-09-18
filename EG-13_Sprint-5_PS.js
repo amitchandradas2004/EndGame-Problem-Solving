83e35627bedf46901052862f3380e1f7cf540c0b
amitchandradas2004 <amitcdas792@gmail.com>
2026-09-18 09:06:10 +0600

    Sprint 5 problem 1 is solved

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -6,8 +6,21 @@
  * @return {boolean}
  */
 var checkSubarraySum = function (nums, k) {
-  // your code goes here
+  if (nums.length === 0) return 0;
+
+  let uniqueIndex = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[uniqueIndex] = nums[i];
+      uniqueIndex++;
+    }
+  }
+
+  return uniqueIndex;
 };
+
+// console.log(checkSubarraySum([1, 1, 2]));
 
 // Expected Input: [1, 1, 2]
 // Expected Output: 2
